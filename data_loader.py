b85099a36e807c66fcee99a4e2a50f85caeca7f9
caidan <caidangray@gmail.com>
2025-03-03 16:49:26 -0700

    Created initial files as well as initial CNN algorithm

---
+++ data_loader.py
@@ -0,0 +1,17 @@
+"""
+NAME: data_loader.py
+DESCRIPTION: python file meant to load dataset of images for sign language recognition
+PROGRAMMER: Caidan Gray
+DATE: 3/3/2025
+"""
+
+import torch
+import torchvision
+import torch.utils.data as data
+import numpy as np
+import math
+
+
+class data_loader():
+    def __init__(self):
+        """"""
