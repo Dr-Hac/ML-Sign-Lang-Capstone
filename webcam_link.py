3d485147eecd60d9a7342644753bc711574ae339
Caidan Gray <caidangray@gmail.com>
2025-04-07 15:24:09 -0600

    added website.py and HTML.html. updated webcam link to add a get_frame function

--- webcam_link.py
+++ webcam_link.py
@@ -3,7 +3,7 @@ NAME: webcam_link.py
 DESCRIPTION: use openCV to get camera access
 PROGRAMMER: Caidan Gray
 CREATION DATE: 4/3/2025
-LAST EDITED: 4/4/2025   (please update each time the script is changed)
+LAST EDITED: 4/7/2025   (please update each time the script is changed)
 """
 
 import cv2
@@ -11,28 +11,40 @@ import cv2
 
 class Webcam_Link():
     def __init__(self, webcam: int):
+        """initialize the webcam link by creating variables and a named window"""
         super(Webcam_Link, self).__init__()
-        cv2.namedWindow("Not-Hotdog")
-        self.vcc = None
+        # create a window
+        cv2.namedWindow("Sign Language Recognition")
+        self.vc = None
         self.webcam = webcam
 
     def attempt_webcam_start(self):
+        """attempt to start the webcam"""
         self.vc = cv2.VideoCapture(self.webcam)
+        # check that the window for the webcam is created
         if self.vc.isOpened(): # try to get the first frame
             rval, frame = self.vc.read()
         else:
             rval = False
 
         while rval:
+            # put the webcam input in the window
             cv2.imshow("preview", frame)
             rval, frame = self.vc.read()
+            # set key to esc key
             key = cv2.waitKey(20)
             if key == 27: # exit on ESC
                 self.stop_webcam()
 
     def stop_webcam(self):
-        cv2.destroyWindow("Not-Hotdog")
+        """stop webcam when esc key is pressed"""
+        cv2.destroyWindow("Sign Language Recognition")
         self.vc.release()
 
+    def get_frame(self):
+        """return the current frame from the webcam"""
+        return self.vc.read()
+
+
 cam = Webcam_Link(0)
-# cam.attempt_webcam_start()
+cam.attempt_webcam_start()
---
+++ website.py
@@ -0,0 +1,11 @@
+"""
+NAME: website.py
+DESCRIPTION: create and run the webcam on a website
+PROGRAMMER: Caidan Gray
+CREATION DATE: 4/7/2025
+LAST EDITED: 4/7/2025   (please update each time the script is changed)
+"""
+
+from flask import flask
+import flask_wtf
+import wtforms
